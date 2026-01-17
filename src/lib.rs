e8d5d48a9607822190b5acccb31a3def013596d2
Ahmadfauzi34 <89245707+Ahmadfauzi34@users.noreply.github.com>
2026-01-17 17:41:54 +0700

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,7 @@
+use burn::prelude::*;
+use wasm_bindgen::prelude::*;
+use burn_ndarray::NdArray;
 
+// 1. KUNCI BACKEND
+// Kita putuskan: Untuk WASM ini, kita pakai CPU (NdArray) dengan float 32-bit.
+type WasmBackend = NdArray<f32>;
