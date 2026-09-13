f5e263d04a432d55349c8420cb8514f1d03b80fe
Ahmadfauzi34 <89245707+Ahmadfauzi34@users.noreply.github.com>
2026-09-13 23:38:21 +0700

    math: export Math Program v4 core

--- src/math/mod.rs
+++ src/math/mod.rs
@@ -4,6 +4,7 @@ pub mod probability;
 pub mod program;
 pub(crate) mod program_select_params;
 pub(crate) mod program_shape_params;
+pub mod program_v4;
 pub(crate) mod program_v4_step;
 pub mod program_wasm;
 pub mod statistics;
@@ -13,6 +14,9 @@ pub use linalg::{linear_algebra_capabilities, WasmLinearAlgebra};
 pub use numeric::{numeric_kernel_capabilities, WasmNumericKernel};
 pub use probability::{probability_capabilities, WasmProbability};
 pub use program::{math_program_capabilities, MathProgram, MathProgramBuilder};
+pub use program_v4::{
+    math_program_v4_capabilities, MathProgramV4, MathProgramV4Builder, OP_SELECT_AXIS,
+};
 pub use program_wasm::{wasm_math_program_capabilities, WasmMathProgram, WasmMathProgramBuilder};
 pub use statistics::{statistics_capabilities, WasmStatistics};
 pub use tensor::{tensor_transform_capabilities, WasmTensorTransform};
