d61c00864c563b2890756e3677632a9715915149
Ahmadfauzi34 <89245707+Ahmadfauzi34@users.noreply.github.com>
2026-09-13 21:03:09 +0700

    math: export Math Program v1 core

--- src/math/mod.rs
+++ src/math/mod.rs
@@ -1,11 +1,13 @@
 pub mod linalg;
 pub mod numeric;
 pub mod probability;
+pub mod program;
 pub mod statistics;
 pub mod tensor;
 
 pub use linalg::{linear_algebra_capabilities, WasmLinearAlgebra};
 pub use numeric::{numeric_kernel_capabilities, WasmNumericKernel};
 pub use probability::{probability_capabilities, WasmProbability};
+pub use program::{math_program_capabilities, MathProgram, MathProgramBuilder};
 pub use statistics::{statistics_capabilities, WasmStatistics};
 pub use tensor::{tensor_transform_capabilities, WasmTensorTransform};
