d16206402533cd69ba7d0f37f9d0c9c44a6ba248
Ahmadfauzi34 <89245707+Ahmadfauzi34@users.noreply.github.com>
2026-09-13 18:42:36 +0700

    feat: export Linear Algebra v1 surface

--- src/math/mod.rs
+++ src/math/mod.rs
@@ -1,5 +1,7 @@
+pub mod linalg;
 pub mod numeric;
 pub mod tensor;
 
+pub use linalg::{linear_algebra_capabilities, WasmLinearAlgebra};
 pub use numeric::{numeric_kernel_capabilities, WasmNumericKernel};
 pub use tensor::{tensor_transform_capabilities, WasmTensorTransform};
