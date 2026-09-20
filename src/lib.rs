b3aa47024c10bbc7d0ebdf7b5ec3d4794e758166
Ahmadfauzi34 <89245707+Ahmadfauzi34@users.noreply.github.com>
2026-09-20 20:42:57 +0700

    feat: export optional input contract module

--- src/lib.rs
+++ src/lib.rs
@@ -16,6 +16,7 @@ pub mod graph_parameters;
 pub mod graph_parameters_wasm;
 pub mod interaction;
 pub mod interaction_fault;
+pub mod input_contract;
 pub mod introspection;
 pub mod layers;
 pub mod math;
